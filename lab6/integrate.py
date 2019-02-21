33e56880c3989a7729689192d9195462ad3373e7
xns1997 <723648649@qq.com>
2019-02-21 11:03:45 -0800

    Update integrate.py

--- lab6/integrate.py
+++ lab6/integrate.py
@@ -9,7 +9,7 @@ def f(x):
 
 def integrate (f,a, b, n = 100):
      x = np.linspace(a,b,n)
-     print(x)
+     #print(x)
      y = []
      interval = (b - a ) / n
      print (interval)
@@ -20,10 +20,23 @@ def integrate (f,a, b, n = 100):
          #print (y[i] , y[i + 1])
          area += (y[i] + y[i + 1]) * interval / 2
      print(area)
+     return area
 
-def integrate_mc(f, a, b, (c, d) ,n  = 1000):
+def integrate_mc(f, a, b,c,d, n  = 1000):
+     count = 0;
+     rec_area = abs(a-b) * abs(c-d)
+
+     for i in range (n):
+         rand_x = np.random.uniform(a, b)
+         rand_y = np.random.uniform(c, d)
+         if rand_y < f(rand_x) and rand_y > 0 and f(rand_x) > 0:
+            count += 1
+         elif rand_y > f(rand_x) and rand_y < 0 and f(rand_x) < 0:
+            count -= 1
+     area = rec_area * count / n
+     print (area)
+     return area
 
 
-
-integrate(1,100,1000)
-
+integrate(f,1,100,100000)
+integrate_mc(f,1,100,1,100000,100000)
