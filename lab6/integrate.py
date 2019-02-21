f7b0470a58c998ba5bde17490a368fc8bff2ad1d
xns1997 <723648649@qq.com>
2019-02-21 13:12:06 -0800

    Update integrate.py

--- lab6/integrate.py
+++ lab6/integrate.py
@@ -30,10 +30,12 @@ def integrate_mc(f, a, b,c,d, n  = 1000):
      for i in range (n):
          rand_x = np.random.uniform(a, b)
          rand_y = np.random.uniform(c, d)
-         if rand_y < f(rand_x) and rand_y > 0 and f(rand_x) > 0:
-            count += 1
-         elif rand_y > f(rand_x) and rand_y < 0 and f(rand_x) < 0:
-            count -= 1
+         if rand_y > 0:
+            if rand_y <= f(rand_x):
+                count += 1
+         else:
+            if rand_y >= f(rand_x):
+                count += 1
      area = rec_area * count / n
      print (area)
      return area
@@ -46,9 +48,9 @@ def approximate_pi(n):
     count = 0
     rec_area = abs(x_max-x_min) * abs(y_max-y_min)
     for i in range(n):
-        x = np.random.uniform(x_min,x_max)
-        y = np.random.uniform(y_min,y_max)
-        if (f(x)+f(y)) < 1:
+        rand_x = np.random.uniform(x_min,x_max)
+        rand_y = np.random.uniform(y_min,y_max)
+        if (f(rand_x)+f(rand_y)) < 1:
             count += 1
         print("Process:{0}%".format(round((i + 1) * 100 / n)), end="\r")
     print("")
